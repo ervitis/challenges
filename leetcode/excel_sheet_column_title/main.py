bcc8052bffb04070151e5e951fb0cd4713fa4a69
ervitis <vitomarti@gmail.com>
2021-02-06 09:46:54 +0100

    feat: excel scheet column title

---
+++ leetcode/excel_sheet_column_title/main.py
@@ -0,0 +1,21 @@
+"""
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+"""
+
+
+def convert_to_title(n: int) -> str:
+    t = ''
+
+    while n > 0:
+        r = n % 26
+        c = chr(ord("A") + r - 1) if r > 0 else "Z"
+        t = c + t
+        n = n // 26 if r > 0 else n // 26 - 1
+    return t
+
+
+if __name__ == '__main__':
+    # print(convert_to_title(1))
+    # print(convert_to_title(2))
+    print(convert_to_title(28))
+    print(convert_to_title(701))
