cd1828f62cd325102785292a287cca4ad4222231
ervitis <vitomarti@gmail.com>
2021-02-15 08:20:19 +0100

    feat: nim game

---
+++ leetcode/nim_game/main.py
@@ -0,0 +1,20 @@
+"""
+You are playing the following Nim Game with your friend:
+
+    Initially, there is a heap of stones on the table.
+    You and your friend will alternate taking turns, and you go first.
+    On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
+    The one who removes the last stone is the winner.
+
+Given n, the number of stones in the heap, return true if you can win the game assuming both you and your friend play optimally, otherwise return false
+"""
+
+
+def can_win_nim(n: int) -> bool:
+    return n % 4 != 0
+
+
+if __name__ == '__main__':
+    print(can_win_nim(4))
+    print(can_win_nim(1))
+    print(can_win_nim(2))
